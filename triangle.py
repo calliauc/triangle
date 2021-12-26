3e07f35eb115eac4dd5ac75596982cdc9491d753
Clément Calliau <34284617+calliauc@users.noreply.github.com>
2021-12-26 23:58:52 +0100

    change rjust to center
    Replace str.rjust() with str.center() to ident stars
    Remove "()" to return (well... Java sequels)
    Passed through Black linter

--- triangle.py
+++ triangle.py
@@ -1,9 +1,10 @@
-
 def triangle(nb):
-    if not nb or nb%2 == 0:
-        return()
+    if not nb or nb % 2 == 0:
+        return
     for i in range(nb):
-        num="*"
-        print(num.rjust(nb-i, ' ') + i*2*"*")
+        stars = (i * 2 + 1) * "*"
+        center = stars.center(nb * 2, " ")
+        print(f"{center}")
+
 
 triangle(5)
